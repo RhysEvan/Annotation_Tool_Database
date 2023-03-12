6057b89b3800be57381077e2309bc83503664f78
RhysEvan <88195825+RhysEvan@users.noreply.github.com>
2023-03-12 19:22:25 +0100

    changes to fix bugs in current research, changes aren't important

--- Annotation-Tool/renamer.py
+++ Annotation-Tool/renamer.py
@@ -1,11 +1,11 @@
 import os
 import glob
-place = r"C:\Users\Rhys Evans\Universiteit Antwerpen\Master Thesis Autostitching - Documenten\General\Skin_Database"
+place = r"C:\Users\mheva\OneDrive\Bureaublad\temp"
 filenames = glob.glob(place + r'/*')
 
-#to_displace = "cut_labeled"
-#to_displace_with = "labeled"
+to_displace = "_.tif"
+to_displace_with = ".tif"
 os.chdir(place)
 for i, fn in enumerate(filenames):
-    os.rename(filenames[i], fn.replace(fn, "synth_skin_"+str(i)+".jpg"))
+    os.rename(filenames[i], fn.replace(to_displace, to_displace_with))
     
